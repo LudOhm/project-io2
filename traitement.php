45eee8cc3471fc7814597903fe2b4530a78b7ed0
LudOhm <ludivine.perrier.babin@gmail.com>
2023-04-21 19:48:02 +0200

    update

--- traitement.php
+++ traitement.php
@@ -1,19 +1,19 @@
 <?php 
     $pdo = new PDO('mysql:host=localhost;dbname=instapets', 'root', 'root');
-    if(isset($_GET['Pseudo'])){
-        $userPseudo= $_GET['Pseudo'];
+    if(isset($_POST['Pseudo'])){
+        $userPseudo= $_POST['Pseudo'];
     } 
-    if(isset($_GET['nom'])){
-        $usernom= $_GET['nom'];
+    if(isset($_POST['nom'])){
+        $usernom= $_POST['nom'];
     }   
-    if(isset($_GET['prenom'])){
-        $userprenom= $_GET['prenom'];
+    if(isset($_POST['prenom'])){
+        $userprenom= $_POST['prenom'];
     }   
-    if(isset($_GET['mail'])){
-        $usermail= $_GET['mail'];
+    if(isset($_POST['mail'])){
+        $usermail= $_POST['mail'];
     }  
-    if(isset($_GET['mdp'])&&isset($_GET['mdp2'])){
-        $userpassword = $_GET['mdp'];
+    if(isset($_POST['mdp'])){
+        $userpassword = $_POST['mdp'];
     }    
 
 ?>
