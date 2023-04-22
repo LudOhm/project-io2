48d4800c8468dd72f40d145bb08c0d57c5763b46
LudOhm <ludivine.perrier.babin@gmail.com>
2023-04-22 14:39:07 +0200

    up database

--- base.sql
+++ base.sql
@@ -13,26 +13,34 @@ CREATE TABLE Users (
 );
 
 CREATE TABLE Followings (
-  id_wing SERIAL PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  id_suis SERIAL PRIMARY KEY NOT NULL AUTO_INCREMENT,
   user_id INTEGER NOT NULL,
   following_id INTEGER NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES Users(user_id),
+  FOREIGN KEY (following_id) REFERENCES Users(user_id),
 )
 
 CREATE TABLE Followers (
-  id_wers SERIAL PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  id_suivi SERIAL PRIMARY KEY NOT NULL AUTO_INCREMENT,
   user_id INTEGER NOT NULL,
   followers_id INTEGER NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES Users(user_id),
+  FOREIGN KEY (followers_id) REFERENCES Users(user_id),
 )
 
 CREATE TABLE Posts (
   post_id SERIAL PRIMARY KEY NOT NULL AUTO_INCREMENT,
   user_id INTEGER NOT NULL,
-  post_contenu VARCHAR(5000) NOT NULL
+  post_title VARCHAR(500) NOT NULL,
+  post_contenu VARCHAR(5000) NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES Users(user_id),
 );
 
 CREATE TABLE Likes(
   like_id SERIAL PRIMARY KEY NOT NULL AUTO_INCREMENT,
   post_id INTEGER NOT NULL,
   user_id INTEGER NOT NULL,
-  is_liked BOOLEAN NOT NULL
+  is_liked BOOLEAN NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES Users(user_id),
+  FOREIGN KEY (post_id) REFERENCES Posts(post_id),
 )
