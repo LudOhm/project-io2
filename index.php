a59c3a785c144a0c3187cdaa0538a0e2474c9ba4
Amel <122884530+Amelkc@users.noreply.github.com>
2023-04-22 15:27:44 +0200

    Create index.php
    Pas du tout terminé mais c'est pour faire comme dans l'ex 4 du TP8 pour une meilleure structure du site

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Profil utilisateur</title>
+</head>
+<body>
+<?php
+include_once('sauvegarde.php');
+include_once('acceuil.php');
+include_once('inscription.php');
+
+?>
