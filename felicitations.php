ffcee218492b99141685b3dcdedc6157b5e061f1
Amel <122884530+Amelkc@users.noreply.github.com>
2023-04-23 18:30:49 +0200

    Update felicitations.php

--- felicitations.php
+++ felicitations.php
@@ -1,1 +1,18 @@
+<?php 
+  function felicitations(){
+   $html = "<html lang=\"fr\"> <head>
+<meta  http-equiv='Content-Type' content='text/html; charset=utf-8'>
+<title>Inscription Réussie!</title>
+ <script src=\"https://kit.fontawesome.com/b1ff4425a2.js\" crossorigin=\"anonymous\"></script>
+</head>
+<body>
+  <h1>Merci pour votre inscription !<h1>
+  <button type=\"submit\" formaction=\"index.php?action=profil\" class=\"btn\"><i class=\"fa-sharp fa-solid fa-cat fa-lg\" style=\"color: #B67645;\"></i>Voir mon profil</button>
+<button type=\"submit\" formaction=\"index.php\" class=\"btn\">
+  <i class=\"fa-sharp fa-solid fa-dog fa-flip-horizontal fa-lg\" style=\"color: #B67645;\"></i>Retour à l'acceuil</button> 
+</body></html>";
+    return $html;
+  }
+
+?>
 
