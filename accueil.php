fb4045848f31a7b86085081e214fc00a9e8af68c
Amel <122884530+Amelkc@users.noreply.github.com>
2023-04-23 15:06:51 +0200

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -1,53 +1,43 @@
-<!DOCTYPE html> 
-<html lang="fr"> 
-    <head>
-        <meta charset="utf-8">
+<?php
+    function display_Acceuil(){
+       $html =  "<html lang=\"fr\"> 
+        <head>
+        <meta  http-equiv='Content-Type' content='text/html; charset=utf-8'>
         <title>InstaPets</title> 
-        <link href="accueil.css" rel="stylesheet">
-    </head>
-<body>
-	<header>
-		<h1>InstaPets</h1>
-	</header>
-
-	<main>
-			<?php if(!isset($loggedUser)): ?>
-				<?php include("bienvenue.php") ?>
-
-			<?php else: ?>
-				<h2>Postes récents</h2>
-			<?php
-				// Connecter à la base de donnée
-				$pdo = new PDO('mysql:host=localhost;dbname=instapets', 'root', 'root');
-
-				$stmt = $pdo->prepare('SELECT Posts.post_title, Posts.post_content, Posts.post_picture, Users.user_pseudo FROM Posts INNER JOIN Users ON Posts.user_id = Users.user_id ORDER BY DESC LIMIT 20');
-				$stmt->execute();
-				$posts = $stmt->fetchAll();
-
-				foreach ($posts as $post) {
-					echo '<article>';
-					echo '<h3>' . htmlspecialchars($post['post_title']) . '</h3>';
-					echo '<p>' . $post['post_picture'] . '</p>';
-					echo '<p>' . htmlspecialchars($post['post_contenu']) . '</p>';
-					echo '<p class="meta">Posted by ' . htmlspecialchars($post['user_id']) . '</p>';
-					echo '</article>';
-				}
-			?>
-		<?php endif; ?>
-	</main>
-
-    <aside>
-        <form action="recherche.php" method="post">
-          <input type="search" name="recherche" placeholder="Rechercher">
-          <input type="submit" value="Ok !">
-        </form>
+        <link href=\"accueil.css\" rel=\"stylesheet\">
+        </head>
+        <body>
+	    <header>
+	    </header>
+	    <main>";
+        if(!isset($loggedUser)){ 
+            $html .= " <h2>Bienvenue sur Instapets, le réseau social pour les détenteurs d'animaux qui veulent partager 
+            et communiquer avec les autres détenteurs d'animaux qui partagent un intérêt commun.</h2><br>
+            <li><a href=\"index.php?action=inscription\">Rejoignez-nous!</a></li><li><a href=\"index.php?action=connexion\">J'ai déjà un compte!</a></li>";
+        }
+        
+		else{
+            $html.="<h2>Recent Posts</h2>";
+            $pdo = new PDO('mysql:host=localhost;dbname=instapets', 'root', 'root');
+		    $stmt = $pdo->prepare('SELECT Posts.post_title, Posts.post_content, Posts.post_picture, Users.user_pseudo FROM Posts INNER JOIN Users ON Posts.user_id = Users.user_id ORDER BY DESC LIMIT 20');
+		    $stmt->execute();
+		    $posts = $stmt->fetchAll();
+			foreach ($posts as $post) {
+				$html .= "<article><h3>" . htmlspecialchars($post['post_title']) . "</h3><p>" . htmlspecialchars($post['post_picture']) . "</p><p>" . htmlspecialchars($post['post_content']) .
+                "</p><p class=\"meta\">Posted by" . htmlspecialchars($post['user_id'])."</p></article>";
+			}
+	
+	    $html = $html. "</main><aside><form action=\"index.php?action=search\" method=\"post\"><input type=\"search\" name=\"q\" placeholder=\"Rechercher\">
+        <input type=\"submit\" value=\"Ok !\"></form>
         <ul>
-          <li><a href="publier.php">Publier</a></li>
-          <li><a href="profil.php">MonCompte</a></li>
+      <li><a href=\"index.php?action=publier\">Publier</a></li>
+      <li><a href=\"index.php?action=profil\">MonCompte</a></li>
         </ul>
-      </aside>
+        </aside>";
+    }
 
-    <?php include("footer.php") ?>
-    
-</body>
-</html>
+        $html = $html."</body></html>";
+ 
+        return $html;
+    }
+?>
