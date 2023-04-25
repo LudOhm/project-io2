f65cd024d98ea51fbaee4c9495fee9f204947d8a
Amel <122884530+Amelkc@users.noreply.github.com>
2023-04-25 16:19:06 +0200

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -10,13 +10,14 @@
 	    <header>
 	    </header>
 	    <main>";
-        if(!isset($_SESSION['LOGGED_USER'])){ 
+        if(!isset($_SESSION['LOGGED_USER'])&&!isset($_COOKIE['LOGGED_USER'])){ 
             $html .= " <h2>Bienvenue sur Instapets, le réseau social pour les détenteurs d'animaux qui veulent partager 
             et communiquer avec les autres détenteurs d'animaux qui partagent un intérêt commun.</h2><br>
             <li><a href=\"index.php?action=inscription\">Rejoignez-nous!</a></li><li><a href=\"index.php?action=connexion\">J'ai déjà un compte!</a></li>";
         }
         
 		else{
+			$user_id = $_COOKIE['LOGGED_USER'];
             $html.="<h2>Recent Posts</h2>";
             $pdo = new PDO('mysql:host=localhost;dbname=instapets', 'root', 'root');
 		    $stmt = $pdo->prepare('SELECT Posts.post_title, Posts.post_content, Posts.post_picture, Users.user_pseudo FROM Posts INNER JOIN Users ON Posts.user_id = Users.user_id ORDER BY DESC LIMIT 20');
