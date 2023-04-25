b5cdaba42d03c914a727c4c8e7fdef8d308e25e8
LudOhm <ludivine.perrier.babin@gmail.com>
2023-04-25 15:42:30 +0200

    update cookies

--- profil.php
+++ profil.php
@@ -35,13 +35,15 @@
         }else{
 		$html .="<h2>Dernière publication</h2>";
     $pdo = new PDO('mysql:host=localhost;dbname=instapets', 'root', 'root');
-    //faire une variable avec l'id ou l'email du user logged et le comparer dans le prepare 
-		$stmt = $pdo->prepare('SELECT Posts.post_title, Posts.post_content, Posts.post_picture, Users.user_pseudo FROM Posts INNER JOIN Users ON Posts.user_id = Users.user_id ORDER BY DESC LIMIT 20');
-		$stmt->execute();
+
+    $user_id = $_COOKIE['LOGGED_USER'];
+    
+		$stmt = $pdo->prepare('SELECT Posts.post_title, Posts.post_content, Posts.post_picture, Users.user_pseudo FROM Posts INNER JOIN Users ON Posts.user_id = ? ORDER BY DESC LIMIT 20');
+		$stmt->execute($user_id);
 		$posts = $stmt->fetchAll();
 		foreach ($posts as $post) {
 				$html .= "<article><h3>" . htmlspecialchars($post['post_title']) . "</h3><p>" . htmlspecialchars($post['post_picture']) . "</p><p>" . htmlspecialchars($post['post_content']) .
-                		"</p><p class=\"meta\">Posted by" . htmlspecialchars($post['user_id'])."</p></article>";
+                		"</p><p class=\"meta\">Posted by" . htmlspecialchars($post['user_pseudo'])."</p></article>";
 				}
 	
 	    	$html = $html. "</main><aside><form action=\"index.php?action=search\" method=\"post\"><input type=\"search\" name=\"q\" placeholder=\"Rechercher\">
--- publier.php
+++ publier.php
@@ -13,8 +13,7 @@
     <?php
     $pdo = new PDO('mysql:host=localhost;dbname=instapets', 'root', 'root');
 
-    // il faut remplace pour mettre une variable qui prend l'id de la personne logged in
-    $user_id = 1;
+    $user_id = $_COOKIE['LOGGED_USER'];
 
 
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
