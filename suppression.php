1b3e07e5d4840102d6fe785f14570524949c7841
Amel <122884530+Amelkc@users.noreply.github.com>
2023-04-25 23:20:41 +0200

    Create suppression.php

---
+++ suppression.php
@@ -0,0 +1,9 @@
+<?php
+/* la fonction peut être appelé depuis chaque endroit où des posts apparaissent soit depuis accueil.php et depuis profil.php
+    gerer cote base;
+    dans les tests il faudra bien verifier que la publication disparait de l'acceuil et du profil
+*/
+  function delete(//jsp encore si y aura des parametres)
+
+
+?>
