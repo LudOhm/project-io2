477e10cfb0387dd7390883b588e4c1becd99ac53
Amel <122884530+Amelkc@users.noreply.github.com>
2023-04-25 13:18:16 +0200

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -1,5 +1,5 @@
 <?php
-    function display_Acceuil(){
+    function display_Accueil(){
        $html =  "<html lang=\"fr\"> 
         <head>
         <meta  http-equiv='Content-Type' content='text/html; charset=utf-8'>
