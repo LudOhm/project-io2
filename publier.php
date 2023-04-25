fb7836f703a88678b388f0478bd689eec3054d52
Amel <122884530+Amelkc@users.noreply.github.com>
2023-04-25 16:07:13 +0200

    Update publier.php

--- publier.php
+++ publier.php
@@ -1,57 +1,55 @@
-<!DOCTYPE html> 
-<html lang="fr"> 
-    <head>
-        <meta charset="utf-8">
-        <title>InstaPets</title> 
-    </head>
-<body> 
-    <header> 
-        <h1>InstaPets</h1>
-        
-    </header>
+<?php
+    function publier(){
+        $html = "<html lang=\"fr\"> <head><meta charset="utf-8">
+        <title>InstaPets</title></head>
+        <body><main>
+        <h2>Nouvelle publication</h2>
+        <form method=\"POST\" enctype=\"multipart/form-data\">
 
-    <?php
-    $pdo = new PDO('mysql:host=localhost;dbname=instapets', 'root', 'root');
+        <label for=\"post_title\">Title:</label>
+        <input type=\"text\" name=\"post_title\" required>
+        
+        <label for=\"post_contenu\">Content:</label>
+        <textarea name=\"post_contenu\" rows=\"5\" required></textarea>
+        
+        <label for=\"post_picture\">Picture:</label>
+        <input type=\"file\" accept=\".jpeg, .jpg, .png, .mp4, .avi, .mov, .flv\" name=\"post_picture\">
+        
+        <button type=\"submit\" name=\"submit\">Publier</button>
+        </form></main></body></html>";
+
+        echo $html;
+
+        $pdo = new PDO('mysql:host=localhost;dbname=instapets', 'root', 'root');
 
     // il faut remplace pour mettre une variable qui prend l'id de la personne logged in
     $user_id = 1;
-
-    // Handle the form submission
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        // Get the post data from the form
+    if(isset ($_POST['submit'])){
         $post_title = $_POST['post_title'];
         $post_contenu = $_POST['post_contenu'];
-
-        // Upload the post picture, if provided
         $post_picture = null;
-        if (isset($_FILES['post_picture']) && $_FILES['post_picture']['error'] === UPLOAD_ERR_OK) {
-            $post_picture = file_get_contents($_FILES['post_picture']['tmp_name']);
-        }
 
-        // les ? sont 
+        if (isset($_FILES['post_picture'])) {
+            $fileName = $_FILES['post_picture']['name'];
+            $fileExtension = explode('.',$fileName);
+            $validExtensions = ['jpg','jpeg','png', 'mp4', 'avi', 'mov', 'flv'];
+            if(!(in_array($fileExtension,$validExtensions))){echo  "<script type=\"text/javascript\">window.alert('Format du fichier invalide')</script>";}
+
+            if($_FILES['post_picture']['error'] === 0) {
+                $post_picture = file_get_contents($_FILES['post_picture']['tmp_name']);
+               
+            } else{
+                echo "<script type=\"text/javascript\">window.alert('Une erreur est survenue, veuillez réessayer')</script>";
+
+            }
+        }
         $stmt = $pdo->prepare('INSERT INTO posts (user_id, post_title, post_contenu, post_picture) VALUES (?, ?, ?, ?)');
         $stmt->execute([$user_id, $post_title, $post_contenu, $post_picture]);
+        echo "<script type=\"text/javascript\">window.alert('Publication publiée avec succès !')</script>";
 
+        }
+       
     }
+
+}
     ?>
-
-<main>
-		<h2>Nouvelle publication</h2>
-		<form method="POST" enctype="multipart/form-data">
-			<label for="post_title">Title:</label>
-			<input type="text" name="post_title" required>
-
-			<label for="post_contenu">Content:</label>
-			<textarea name="post_contenu" rows="5" required></textarea>
-
-			<label for="post_picture">Picture:</label>
-			<input type="file" name="post_picture">
-
-			<input type="submit" value="Create Post">
-		</form>
-	</main>
-
-    
-    
-</body>
-</html>
