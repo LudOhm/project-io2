f0cb2caef560b9293496aed3e701fa85fd18c463
Amel <122884530+Amelkc@users.noreply.github.com>
2023-04-25 16:29:23 +0200

    Update traitementConnexion.php

--- traitementConnexion.php
+++ traitementConnexion.php
@@ -11,9 +11,11 @@
             foreach ($user as $users) {
                 if (( $users['user_email'] === $_POST['login'] ||  $users['user_pseudo'] === $_POST['login'] ) &&
                 $users['user_motdepasse'] === $_POST['mdp']) {
-                        $loggedUser = [
-                            'mail' => $users['user_mail']
-                        ];
+                 $_SESSION['LOGGED_USER']= $_POST['user_id'];
+                     $loggedUser = [
+                         'user_id' => $_POST['user_id']
+                     ];
+                     setcookie('LOGGED_USER', $_SESSION['LOGGED_USER'], time()+3600, '/');
                     return true;
                 } 
             } 
