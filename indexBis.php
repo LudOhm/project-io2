8f2f076b35fc69122fb0d1a32944c37c162af24c
Amel <122884530+Amelkc@users.noreply.github.com>
2023-04-27 19:11:34 +0200

    BIG UPDATE

--- indexBis.php
+++ indexBis.php
@@ -1,46 +1,94 @@
-<?php
-    session_start();
-    include_once('inscription.php');
-    include_once('bienvenue.php');
-    //include_once('traitementInscription.php');
-    include_once('connexion.php');
-   // include_once('traitementConnexion.php');
-    if(!isset($_SESSION['LOGGED_MDP'])){
-        if(isset($_GET['action'])){
-            switch($_GET['action']){
-                case 'bienvenue' :
-                    $fonction = display_Bienvenue();
+<?php session_start();
+include_once('inscription.php');
+include_once('bienvenue.php');
+include_once('traitementInscription.php');
+include_once('connexion.php');
+include_once('traitementConnexion.php');
+include_once('accueil.php');
+include_once('felicitations.php');
+include_once('publier.php');
+include_once('recherche.php');
+include_once('profil.php');
+include_once('abonnerment.php');
+
+if(!isset($_SESSION['LOGGED_MDP'])){
+    if(isset($_GET['action'])){
+        switch($_GET['action']){
+            case 'bienvenue' :
+                echo display_Bienvenue();
+                break;
+            case 'inscription' :
+                echo print_form(false);
+                break;
+            case 'sauvergarder' :
+                if(inscriptionValidee()){
+                    echo felicitations(false);
                     break;
-                case 'inscription' :
-                    $fonction = print_form(false);
+                }else{
+                    echo print_form(false);
                     break;
-                case 'connexion' :
-                    $fonction = print_Login();
+                    }
+            case 'connexion' :
+                echo print_Login();
+                break;
+            case 'check' :
+                if(connexion_check()){
+                    echo display_Accueil();
                     break;
-                 /*case 'sauvergarder' :
-                    if(inscriptionValidee()){
-                        echo felicitations(false);
-                        break;
-                    }else{
-                        echo print_form(false);
-                        break;
-                        }
-                
-               case 'check' :
-                    if(connexion_check()){
-                        echo display_Accueil();
-                        break;
-                    }else {
-                        echo print_Login();
-                        break;
-                    }*/
-                default : $fonction = display_Bienvenue();
-            }
-        }else{
-            $fonction = display_Bienvenue();
+                }else {
+                    echo print_Login();
+                    break;
+                }
+            default : header('Location: index.php?action=bienvenue'); exit();
         }
+    }else{
+        echo display_Bienvenue();
     }
+    
+}
+else{
+if(!isset ($_GET['action'])){
+    echo display_Accueil(); // si le script arrive ici c'est que l'user est connecté
+}else{
+    switch($_GET['action']){
+        case 'modifier' :
+            echo print_form(true); // a ameliorer (preremplissage des champs sauf mdp pr plus de sécurité?)
+            break;
+        case 'update' :
+            if(modificationValidee()){ //À CREER
+                echo felicitations(true);
+                break;
+            } else{
+                echo print_form(true);
+            }
+	    case 'publier' :
+            publier() ; // je dois revenir dessus
+            break;
+	    case 'search' :
+            search(); // je dois revenir dessus
+            break;
+        case 'profil' :
+	        $id=$_GET['id'];
+            echo display_profil($id);
+            break;
+        case 'unfollow':
+            $id=$_GET['id'];
+            unFollow($id);
+            echo display_profil($id);
+            break;
+        case 'subscribe':
+            $id=$_GET['id'];
+            follow($id);
+            echo display_profil($id);
+            break;
+       
+        default :
+          echo display_Accueil();  
+    }
+}
+}
 ?>
+
 
 <html>
 <head>
@@ -49,7 +97,6 @@
 </head>
 <body>
 <h1> Insta Pets</h1>
-<main><?php echo $fonction?></main>
 <footer>
 		<p>&copy; 2023 InstaPets</p>
         <p>KACI Amel & PERRIER-BABIN Ludivine</p>
