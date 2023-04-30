65c95895da587496debbc6f3df7bc4b2d9996285
Amel <122884530+Amelkc@users.noreply.github.com>
2023-04-30 14:40:34 +0200

    Update profil.php

--- profil.php
+++ profil.php
@@ -63,7 +63,7 @@
         		} 
 		}
 	// en dessous je vais m'occuper d'un peu réorganiser tout ça je fais une petite pause
-	  $html = $html. "</main><aside><form action=\"index.php?action=search\" method=\"post\"><input type=\"search\" name=\"q\" placeholder=\"Rechercher\">
+	  $html = $html. "</main><aside><form action=\"index.php?action=search\" method=\"get\"><input type=\"search\" name=\"q\" placeholder=\"Rechercher\">
   		<input type=\"submit\" value=\"Ok !\"></form> 
         	<ul>
         	<li><a href=\"index.php?action=publier\">Publier</a></li>
