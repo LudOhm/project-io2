d1a4f3a195b625e2aafec6a0ddd1dbbe691d6b49
LudOhm <ludivine.perrier.babin@gmail.com>
2023-04-30 09:29:23 +0200

    update publier

--- publier.php
+++ publier.php
@@ -21,39 +21,39 @@
 
         $pdo = new PDO('mysql:host=localhost;dbname=instapets', 'root', 'root');
 
-    $user_id = $_SESSION['LOGGED_ID'];;
-    if(isset ($_POST['submit'])){
-        $post_title = $_POST['post_title'];
-        $post_contenu = $_POST['post_contenu'];
-        $post_picture = null;
+        $user_id = $_SESSION['LOGGED_ID'];;
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            $post_title = $_POST['post_title'];
+            $post_contenu = $_POST['post_contenu'];
+            $post_picture = null;
 
 
-        if (!empty($_FILES['post_picture'])) {
-            $fileName = $_FILES['post_picture']['name'];
-            $fileExtension = explode('.',$fileName);
-            $validExtensions = ['jpg','jpeg','png', 'mp4', 'avi', 'mov', 'flv'];
-            if(!(in_array($fileExtension,$validExtensions))){
-                $html.=  "<script type=\"text/javascript\">window.alert('Format du fichier invalide')</script><button type=\"button\"><a href=\"index.php?action=publier\">Réessayer</a></button></body></html>";
-                return $html;
+            if (isset($_FILES['post_picture']['error']) && $_FILES['post_picture']['error'] === UPLOAD_ERR_OK) {
+                $fileName = $_FILES['post_picture']['name'];
+                $fileExtension = explode('.',$fileName);
+                $validExtensions = ['jpg','jpeg','png', 'mp4', 'avi', 'mov', 'flv'];
+                if(!(in_array($fileExtension,$validExtensions))){
+                    $html.=  "<script type=\"text/javascript\">window.alert('Format du fichier invalide')</script><button type=\"button\"><a href=\"index.php?action=publier\">Réessayer</a></button></body></html>";
+                    return $html;
+                }
+
+                if($_FILES['post_picture']['error'] === 0) {
+                    $post_picture = file_get_contents($_FILES['post_picture']['tmp_name']);
+                
+                } else{
+                    $html.= "<script type=\"text/javascript\">window.alert('Une erreur est survenue, veuillez réessayer')</script><button type=\"button\"><a href=\"index.php?action=publier\">Réessayer</a></button></body></html>";
+                    return $html;
+
+                }
             }
-
-            if($_FILES['post_picture']['error'] === 0) {
-                $post_picture = file_get_contents($_FILES['post_picture']['tmp_name']);
-               
-            } else{
-                $html.= "<script type=\"text/javascript\">window.alert('Une erreur est survenue, veuillez réessayer')</script><button type=\"button\"><a href=\"index.php?action=publier\">Réessayer</a></button></body></html>";
-                return $html;
-
-            }
-        }
-        $stmt = $pdo->prepare('INSERT INTO posts (user_id, post_title, post_contenu, post_picture) VALUES (?, ?, ?, ?)');
-        $stmt->execute([$user_id, $post_title, $post_contenu, $post_picture]);
-        $html.= "<script type=\"text/javascript\">window.alert('Publication publiée avec succès !')</script><button type=\"button\"><a href=\"index.php\">Retour à mon fil d'actualité</a></button></body></html>";
-        return $html;
-        //A REPRENDRE UN PEU SURTOUT POUR GERER LES REDIRECTIONS 
+            $stmt = $pdo->prepare('INSERT INTO Posts (user_id, post_title, post_contenu, post_picture) VALUES (?, ?, ?, ?)');
+            $stmt->execute([$user_id, $post_title, $post_contenu, $post_picture]);
+            $html.= "<script type=\"text/javascript\">window.alert('Publication publiée avec succès !')</script><button type=\"button\"><a href=\"index.php\">Retour à mon fil d'actualité</a></button></body></html>";
+            return $html;
+            //A REPRENDRE UN PEU SURTOUT POUR GERER LES REDIRECTIONS 
 
         }
-        //pour test mais du coup ça ne met pas dans la base de donnée alors que il peut y avoir les erreurs lié a la photo
+        //pour test mais du coup ça ne met pas dans la base de donnée jsp pq alors que le script javascript s'affiche bien
         return $html;
        
     }   
