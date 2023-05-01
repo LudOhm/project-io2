a6c5fabce34416859c7fc4bb0bf0dfb5a7fd9fe4
Amel <122884530+Amelkc@users.noreply.github.com>
2023-05-01 14:38:21 +0200

    Update recherche.php

--- recherche.php
+++ recherche.php
@@ -1,19 +1,15 @@
 <?php
     function search($user){
     
-        $pdo = new PDO('mysql:host=localhost;dbname=instapets', 'root', 'root');
-        $stmt = $pdo->prepare('SELECT user_pseudo,user_id FROM `Users` ORDER BY id DESC');
-		$stmt->execute();
-		$utilisateurs = $stmt->fetchAll();
-        
-           
-	    $html = "<h2>Résultat pour '".$user."'</h2>";
-            $stmt = $pdo->prepare('SELECT user_pseudo,user_id FROM `Users` WHERE user_pseudo LIKE ? ORDER BY id DESC'); 
+	        $html = "<h2>Résultat pour '".$user."'</h2>";
+        	$pdo = new PDO('mysql:host=localhost;dbname=instapets', 'root', 'root');
+	
+            $stmt = $pdo->prepare('SELECT user_pseudo, user_id FROM `Users` WHERE user_pseudo LIKE ? ORDER BY id DESC'); 
             $stmt->execute(array($user));
 	    $utilisateurs = $stmt->fetchAll();
 
        
-        if(count($utilisateurs) > 0){
+        if($stmt->rowCount() > 0){
             	foreach($utilisateurs as $ut){
             		$html.= "<li><a href=\"index.php?action=profil&amp;id=" .$ut['user_id']."\" >".$ut['user_pseudo']."</a>";
 	   	 }
