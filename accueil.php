e4d99ef5253709fae9fcd0e65074df91758723e8
LudOhm <ludivine.perrier.babin@gmail.com>
2023-05-01 12:33:12 +0200

    update affichage posts

--- accueil.php
+++ accueil.php
@@ -22,16 +22,18 @@ include_once('traitementLikes.php');
 		$stmt = $pdo->prepare('SELECT Posts.post_title, Posts.post_contenu, Posts.post_picture, Posts.post_id, Users.user_pseudo, Users.user_id
 		FROM Posts 
 		INNER JOIN Users ON Posts.user_id = Users.user_id 
-		INNER JOIN Followings ON Posts.user_id = Followings.following_id
-		WHERE Followings.user_id = ? 
+		LEFT JOIN Followings ON Posts.user_id = Followings.following_id AND Followings.user_id = ?
+    	WHERE Posts.user_id = ? OR Followings.following_id IS NOT NULL
 		ORDER BY Posts.post_id DESC 
 		LIMIT 20
 		');
-		$stmt->execute([$_SESSION['LOGGED_ID']]);
+		$stmt->execute([$_SESSION['LOGGED_ID'], $_SESSION['LOGGED_ID']]);
 		$posts = $stmt->fetchAll();
 		foreach ($posts as $post) {
 			$html .= "<article><h3>" . htmlspecialchars($post['post_title']) . "</h3><p>" ;
-			$html .= "<img src=\"data:image/jpeg;base64," . base64_encode($post['post_picture']). "\" alt=\"Post Picture\"><br></p><p>";
+			if($post['post_picture']!== null){
+				$html .= "<img src=\"data:image/jpeg;base64," . base64_encode($post['post_picture']). "\" alt=\"Post Picture\" ><br></p><p>";
+		  	}
 			$html .= htmlspecialchars($post['post_contenu']) . "</p><p class=\"meta\">Posted by <a href=\"index.php?action=profil&amp;id=".$post['user_id']."\">" . htmlspecialchars($post['user_pseudo'])."</a></p></article>";
 			$mot = countPostLikes($post['post_id']) > 1 ? " likes" : " like";
 			$html.= countPostLikes($post['post_id']) . $mot;
