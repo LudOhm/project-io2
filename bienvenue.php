de471e9c80fd00d9f9345144b03b8b657f0e3910
Amel <122884530+Amelkc@users.noreply.github.com>
2023-05-03 01:55:55 +0200

    Update bienvenue.php

--- bienvenue.php
+++ bienvenue.php
@@ -1,13 +1,5 @@
 <?php function display_Bienvenue(){
-    $html =  "<html lang=\"fr\"> 
-    <head>
-    <meta  http-equiv='Content-Type' content='text/html; charset=utf-8'>
-    <title>InstaPets</title> 
-    <link href=\"accueil.css\" rel=\"stylesheet\">
-    </head>
-    <body>
-    <header>
-    </header>
+    $html =  "
     <main>
     <h2>Bienvenue sur Instapets, le réseau social pour les détenteurs d'animaux qui veulent partager 
     et communiquer avec les autres détenteurs d'animaux qui partagent un intérêt commun.</h2><br>
