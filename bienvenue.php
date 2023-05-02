f846332c504487bddea388fa8b7910524b9b8042
Amel <122884530+Amelkc@users.noreply.github.com>
2023-05-03 01:56:14 +0200

    Update bienvenue.php

--- bienvenue.php
+++ bienvenue.php
@@ -3,7 +3,7 @@
     <main>
     <h2>Bienvenue sur Instapets, le réseau social pour les détenteurs d'animaux qui veulent partager 
     et communiquer avec les autres détenteurs d'animaux qui partagent un intérêt commun.</h2><br>
-    <li><a href=\"index.php?action=inscription\">Rejoignez-nous!</a></li><li><a href=\"index.php?action=connexion\">J'ai déjà un compte!</a></li>";
+    <li><a href=\"index.php?action=inscription\">Rejoignez-nous!</a></li><li><a href=\"index.php?action=connexion\">J'ai déjà un compte!</a></li></main>";
 
     return $html;
 }
