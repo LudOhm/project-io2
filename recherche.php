9e42480afe7f657bc99af072f26abb4f8b1f4bd1
Amel <122884530+Amelkc@users.noreply.github.com>
2023-05-03 19:12:17 +0200

    Update recherche.php

--- recherche.php
+++ recherche.php
@@ -14,21 +14,21 @@
 			$utilisateurs = $stmt->fetchAll();
 		}
            
-	    $html = "<h2>Résultat pour '".$user."'</h2>";
+	    $html = "<main><h2>Résultat pour '".$user."'</h2>";
        
         if(isset($utilisateurs) && count($utilisateurs) > 0){
             foreach($utilisateurs as $ut){
             	$html.= "<li><a href=\"index.php?action=profil&amp;id=" .$ut['user_id']."\" >".$ut['user_pseudo']."</a></li>";
 	   		}
-			$html.= "<br>
-			<form action=\"index.php?action=search\" method=\"post\"><input type=\"search\" name=\"q\" placeholder=\"Rechercher\">
+			$html.= "</main><br>
+			<aside><form action=\"index.php?action=search\" method=\"post\"><input type=\"search\" name=\"q\" placeholder=\"Rechercher\">
 			<label for=\"recherche\">Modifier ma recherche :</label><input type=\"submit\" value=\"Ok !\">
-			</form><aside><a href = \"index.php\">Retour à l'accueil</a></aside>";
+			</form><a href = \"index.php\">Retour à l'accueil</a></aside>";
         } else{
-           $html.= "<h3> Aucun utilisateur trouvé</h3>
-			<form action=\"index.php?action=search\" method=\"post\"><input type=\"search\" name=\"q\" placeholder=\"Rechercher\">
+           $html.= "<h3> Aucun utilisateur trouvé</h3></main>
+			<aside><form action=\"index.php?action=search\" method=\"post\"><input type=\"search\" name=\"q\" placeholder=\"Rechercher\">
 			<label for=\"recherche\">Modifier ma recherche :</label><input type=\"submit\" value=\"Ok !\">
-			</form><aside><a href = \"index.php\">Retour à l'accueil</a></aside>";
+			</form><a href = \"index.php\">Retour à l'accueil</a></aside>";
         }
        return $html;
     }
